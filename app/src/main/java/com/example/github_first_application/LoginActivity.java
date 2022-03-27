bfc08614547dc32a2544d40f86b261af5fbe6ae7
velladhurai <velladhurai22@gmail.com>
2022-03-27 23:28:40 +0530

    constarint

--- app/src/main/java/com/example/github_first_application/LoginActivity.java
+++ app/src/main/java/com/example/github_first_application/LoginActivity.java
@@ -1,17 +1,13 @@
 package com.example.github_first_application;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toast.makeText(getApplicationContext(),"First github project",Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_login);
     }
 }
