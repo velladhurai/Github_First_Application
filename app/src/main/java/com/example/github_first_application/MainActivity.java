a1a6765f62695e5b9a2ef8ca08101a329cc62af6
velladhurai <velladhurai22@gmail.com>
2022-03-27 23:33:13 +0530

    consraint_toast

--- app/src/main/java/com/example/github_first_application/MainActivity.java
+++ app/src/main/java/com/example/github_first_application/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(getApplicationContext(),"First github project",Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"velladhurai check",Toast.LENGTH_LONG).show();
     }
 }
